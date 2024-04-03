ebcc6ef77f3c710fa69e1dfbe646e944d4b9f4de
nelstran <nelsonquoc2003@yahoo.com>
2024-04-02 21:00:18 -0600

    Changed pubsec name and added session model

---
+++ lib/MVVM/session_model.dart
@@ -0,0 +1,28 @@
+// import 'dart:ui';
+// import 'dart:math';
+
+class Session{
+  final String field;
+  final int level;
+  final String topic;
+  int dist;
+  int currNum;
+  int maxNum;
+  // Color color;
+
+
+  Session({
+    required this.field,
+    required this.level,
+    required this.topic,
+    int? dist,
+    int? currNum,
+    int? maxNum,
+    }) 
+    : 
+    dist = dist ?? 0,
+    // dist = dist ?? 10 + Random().nextInt(100 - 10 + 1),
+    currNum = currNum ?? 1,
+    maxNum = maxNum ?? 0;
+    // color = colorLibrary.addField(field); // Group sessions by the Field they're in
+}
