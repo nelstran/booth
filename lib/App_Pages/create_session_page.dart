5362b610bd4cc850532c4cce9fa7df007168efc8
jg995 <guillenjoel850@gmail.com>
2024-04-05 16:45:50 -0600

    Initial SessionPage and CreateSessionPage Created

--- lib/App_Pages/create_session_page.dart
+++ lib/App_Pages/create_session_page.dart
@@ -1,4 +1,3 @@
-// TODO: 
 // Create textboxes for user to fill in (can use TextBox from UI components)
 // Should there be required fields that must be filled before they can create a session?
 // If so, make sure required fields are filled in
@@ -10,3 +9,196 @@
 
 // Create a button to add the session to the session home page (can use Button from UI components)
 // Once this button is pressed, go to the session home page
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class CreateSessionPage extends StatefulWidget {
+  const CreateSessionPage({super.key});
+
+  @override
+  _CreateSessionPageState createState() => _CreateSessionPageState();
+}
+
+class _CreateSessionPageState extends State<CreateSessionPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _title;
+  String? _description;
+  String? _time;
+  String? _locationDescription;
+  int? _seatsAvailable;
+  String? _subject;
+  bool _isPublic = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Booth'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Create Session',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _title = value,
+              ),
+              const SizedBox(height: 8.0),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Description'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _description = value,
+              ),
+              const SizedBox(height: 8.0),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Time'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a time';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _time = value,
+              ),
+              const SizedBox(height: 8.0),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Location Description'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a location description';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _locationDescription = value,
+              ),
+              const SizedBox(height: 8.0),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Seats Available'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the number of seats available';
+                  }
+                  final seats = int.tryParse(value);
+                  if (seats == null || seats <= 0) {
+                    return 'Please enter a valid number of seats';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+                onSaved: (value) => _seatsAvailable = int.tryParse(value!),
+              ),
+              const SizedBox(height: 8.0),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Subject'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a subject';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _subject = value,
+              ),
+              const SizedBox(height: 8.0),
+              DropdownButtonFormField<bool>(
+                value: _isPublic,
+                onChanged: (value) {
+                  setState(() {
+                    _isPublic = value!;
+                  });
+                },
+                items: const [
+                  DropdownMenuItem<bool>(
+                    value: true,
+                    child: Text('Public'),
+                  ),
+                  DropdownMenuItem<bool>(
+                    value: false,
+                    child: Text('Private'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    final boothSession = BoothSession(
+                      title: _title!,
+                      description: _description!,
+                      time: _time!,
+                      locationDescription: _locationDescription!,
+                      seatsAvailable: _seatsAvailable!,
+                      subject: _subject!,
+                      isPublic: _isPublic,
+                    );
+                    final databaseReference = FirebaseDatabase.instance.reference();
+                    databaseReference.child('booth_sessions').push().set(boothSession.toJson());
+                    Navigator.pop(context);
+                  }
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {return Colors.blue;}),
+                ),
+                child: const Text('Create Session'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Represents a booth session
+class BoothSession {
+  final String title;
+  final String description;
+  final String time;
+  final String locationDescription;
+  final int seatsAvailable;
+  final String subject;
+  final bool isPublic;
+
+  BoothSession({
+    required this.title,
+    required this.description,
+    required this.time,
+    required this.locationDescription,
+    required this.seatsAvailable,
+    required this.subject,
+    required this.isPublic,
+  });
+
+  /// Converts the booth session to a JSON format
+  Map<String, dynamic> toJson() {
+    return {
+      'title' : title,
+      'description': description,
+      'time': time,
+      'locationDescription': locationDescription,
+      'seatsAvailable': seatsAvailable,
+      'subject': subject,
+      'isPublic': isPublic,
+    };
+  }
+}
