837ab1f7ffa5fc2ccd43faa69851d249fe705f03
Leena Butt <u1283933@gcloud.utah.edu>
2024-10-09 16:29:34 -0600

    testing queries for analytics

--- lib/App_Pages/usage_page.dart
+++ lib/App_Pages/usage_page.dart
@@ -1,28 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_application_1/MVC/booth_controller.dart';
+import 'package:flutter_application_1/MVC/analytics_extension.dart';
+import 'package:intl/intl.dart';
 
 class UsagePage extends StatelessWidget {
   final BoothController controller;
   const UsagePage(this.controller, {super.key});
 
-  // @override
-  // Widget build(BuildContext context) {
-  //    return Column(
-  //      children: [
-  //        Expanded(
-  //         child: Image.asset(
-  //                   'assets/images/usage.png'),
-  //            ),
-  //      ],
-  //    );
-  //   // return const Text("Usage Placeholder");
-  // }
-
   
   @override
   Widget build(BuildContext context) {
    List<double> weeklySummary = [4, 5, 7, 2, 1, 8, 3];
+   String userKey;
+   userKey = "wUxLN0owVqZGEIBeMOt9q6lVBzL2";
 
    BarData myBarData = BarData(
     sunAmount: weeklySummary[0],
@@ -36,48 +29,121 @@ class UsagePage extends StatelessWidget {
 
    myBarData.initializeBarData();
 
+
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          height: 200,
-          child: BarChart(
-            BarChartData(
-              maxY: 24,
-              minY: 0,
-              gridData: FlGridData(show: false),
-              borderData: FlBorderData(show: false),
-              titlesData: FlTitlesData(
-                show: true,
-                topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-                leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-                rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-                bottomTitles: AxisTitles(sideTitles: SideTitles(
-                  showTitles: true, 
-                  getTitlesWidget: getBottomTitles,
-                ),
-                ),
-              ),
-              barGroups: myBarData.barData.map((data) => BarChartGroupData(
-                x: data.x,
-                barRods: [
-                  BarChartRodData(
-                    toY: data.y, 
-                    color: Colors.blue,
-                    width:25,
-                    //borderRadius: BorderRadiues.circular(4),
-                    backDrawRodData: BackgroundBarChartRodData(
-                      show: true,
-                      toY: 24,
-                      color: Colors.grey[200],
-                    ),
-                  ),
-                ],
-              ),).toList(),
-            ),
-          ),
-        ),
-      ),
+       body: FutureBuilder<Map<dynamic, dynamic>>(
+        future: controller.fetchUserStudyData(userKey), 
+        
+        builder: (context, snapshot) {
+           if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No data found.'));
+          }
+          final studyData = snapshot.data!;
+
+          //////////////////////////////////////////////////////////////// NEW CODE FOR QUERYING DATA 
+
+          final startRange = getStartOfWeek(DateTime.now());
+          final endRange = getEndOfWeek(DateTime.now());
+          
+          FirebaseFirestore firestore = FirebaseFirestore.instance;
+          
+          firestore.collection('session_logs')
+                    // instead of startRange and endRange, pass in dates to what we actually have 
+                    // i tried "2024-21-09" as start and "2024-26-09" as end
+                    .where('start_timestamp', isEqualTo: startRange)
+                    .where('end_timestamp', isEqualTo: endRange)
+                    .get()
+                    .then(
+                      (querySnapshot) {
+                        print("success");
+                        for (var docSnapshot in querySnapshot.docs) {
+                          // This should print the data the query retrieves
+                          print('${docSnapshot.id} => ${docSnapshot.data()}');
+                        }
+                      },
+                      onError: (e) => print("Error completing: $e"),
+                    );
+
+          //////////////////////////////////////////////////////////////////////////////////////////////
+         
+          return ListView.builder(
+              itemCount:  studyData.length,
+              itemBuilder: (context, index) {
+                
+                final studySessionID = studyData.keys.elementAt(index);
+                final studyMap = studyData[studySessionID];
+
+                DateTime sessionStart = new DateFormat("yyyy-MM-dd hh:mm:ss").parse(studyMap["start_timestamp"]);
+                DateTime sessionEnd = new DateFormat("yyyy-MM-dd hh:mm:ss").parse(studyMap["end_timestamp"]);
+                final sessionDuration = sessionEnd.difference(sessionStart);
+
+                return ListTile(
+                  title: Text("start:" + studyMap["start_timestamp"].toString() + " end:" + studyMap["end_timestamp"].toString() + "  duration:" + sessionDuration.toString())
+                  );
+              }
+          );
+        }
+       )
     );
+
+  
+
+    // return Scaffold(
+    //   body: Center(
+    //     child: SizedBox(
+    //       height: 200,
+    //       child: BarChart(
+    //         BarChartData(
+    //           maxY: 24,
+    //           minY: 0,
+    //           gridData: FlGridData(show: false),
+    //           borderData: FlBorderData(show: false),
+    //           titlesData: FlTitlesData(
+    //             show: true,
+    //             topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+    //             leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+    //             rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+    //             bottomTitles: AxisTitles(sideTitles: SideTitles(
+    //               showTitles: true, 
+    //               getTitlesWidget: getBottomTitles,
+    //             ),
+    //             ),
+    //           ),
+    //           barGroups: myBarData.barData.map((data) => BarChartGroupData(
+    //             x: data.x,
+    //             barRods: [
+    //               BarChartRodData(
+    //                 toY: data.y, 
+    //                 color: Colors.blue,
+    //                 width:25,
+    //                 //borderRadius: BorderRadiues.circular(4),
+    //                 backDrawRodData: BackgroundBarChartRodData(
+    //                   show: true,
+    //                   toY: 24,
+    //                   color: Colors.grey[200],
+    //                 ),
+    //               ),
+    //             ],
+    //           ),).toList(),
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
+  }
+
+  DateTime getStartOfWeek(DateTime date) {
+    int weekDay = date.weekday;
+    return date.subtract(Duration(days: weekDay - 1));
+  }
+
+  DateTime getEndOfWeek(DateTime date) {
+    int weekDay = date.weekday;
+    return date.add(Duration(days: 7 - weekDay));
   }
 
   Widget getBottomTitles (double value, TitleMeta meta){
