f7080c78622a4d6a247011e4cdd782ed8b5a90ec
Brayden F. Neal <93968829+braydenfneal@users.noreply.github.com>
2024-11-19 19:11:18 -0700

    Fixed PDF iOS bug

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -11,6 +11,8 @@ list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_windows
   geolocator_windows
   local_auth_windows
+  permission_handler_windows
+  syncfusion_pdfviewer_windows
   url_launcher_windows
 )
 
